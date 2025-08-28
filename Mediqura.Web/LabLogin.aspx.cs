6fef4bfaca6e9a1c7c05aadcf2bdca22b72b0669
Robert M <robert@mobimp.com>
2025-08-28 19:33:32 +0530

    license update

--- Mediqura.Web/LabLogin.aspx.cs
+++ Mediqura.Web/LabLogin.aspx.cs
@@ -24,7 +24,7 @@ namespace Mediqura.Web
         protected void Page_Load(object sender, EventArgs e)
         {
             DateTime Today = DateTime.Today;
-            ExpiryDate = Convert.ToDateTime("2025/08/28 12:00:00");
+            ExpiryDate = Convert.ToDateTime("2026/08/28 12:00:00");
             if ((ExpiryDate - Today).TotalDays <= 30)
             {
                 lblmessage.Visible = true;
